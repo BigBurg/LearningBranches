fd77333fe353cbeedc4c1a419f30dd533a133d10
BigBurg <52430691+BigBurg@users.noreply.github.com>
2019-07-01 18:06:00 -0500

    Create printStuff.php
    This is the initial "project"

---
+++ printStuff.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+  ToDo:
+    Make array output
+    Make number output a letter
+    New Line when printing an array
+    Salt output of single string
+    pepper output of single string
+    output count of the array line used.
+**/
+
+echo "hello world";
+
+
+?>
