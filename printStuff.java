98c999dc07f9b107197f571d1bf79c599f9df17c
BigBurg <52430691+BigBurg@users.noreply.github.com>
2019-07-01 18:43:37 -0500

    Update printStuff.java
    New Line when printing an array
    Make number output a letter

--- printStuff.java
+++ printStuff.java
@@ -2,9 +2,6 @@ public class printStuff{
 import static java.lang.System.out;
 /**
   ToDo:
-    Make array output
-    Make number output a letter
-    New Line when printing an array
     Salt output of single string
     pepper output of single string
     output count of the array line used.
@@ -13,10 +10,11 @@ import static java.lang.System.out;
   print("hello world");
   String[] wordArray = ['a','b','c'];
   print(wordArray);
+  print(3);
   
   public static void print(String inStr)
   {
-    System.out.print(inStr);
+    System.out.println(inStr);
   }
 
   
@@ -26,6 +24,18 @@ import static java.lang.System.out;
     {
       print(inArr[i]); 
     }
+  }
+  
+  public static void print(int inNum)
+  {
+    String[] abcs = ["a","b","c","d","e",
+                     "f","g","h","i","j",
+                     "k","l","m","n","o",
+                     "p","q","r","s","t",
+                     "u","v","w","x","y",
+                     "z"];
+    inNum = inNum%26;
+    print(abcs[inNum]);
   }
   
 }
